ee130dccaac7f21b2a14f7b909278b0f4eca1508
sbansal <>
2003-01-22 20:17:54 +0000

    Added Doxygen documentation.

--- GRB/src/LatGRBAlert/BackgroundMixer.h
+++ GRB/src/LatGRBAlert/BackgroundMixer.h
@@ -1,6 +1,16 @@
-// File: BackgroundMixer.h
-//
-//
+/*!
+* \class BackgroundMixer
+*
+* \brief This class provides interface needed to create the background mixed events data.
+* \author Jay Norris        jnorris@lheapop.gsfc.nasa.gov
+* \author Sandhia Bansal    sandhiab@lheapop.gsfc.nasa.gov
+*
+* This class implements methods to create background mixed data from specified events and
+* background data.  Alternatively, it provides methods to read already mixed data.
+*
+*/
+
+
 #ifndef BACKGROUND_MIXER_H
 #define BACKGROUND_MIXER_H
 
@@ -14,54 +24,82 @@
 
 class BackgroundMixer
 {
- public:
-	 // Constructors
-	 BackgroundMixer(const std::string &grbFile, const std::string &backgroundFile, const std::string &mixedDir, 
-		 const double grbOffsetTime);
-	 BackgroundMixer(const std::string &mixedFile);
+public:
+    /*!
+     * \brief Constructors.
+     */
+    BackgroundMixer(const std::string &grbFile, const std::string &backgroundFile, 
+        const std::string &mixedDir, const double grbOffsetTime);
+    BackgroundMixer(const std::string &mixedFile);
+    
 
-	  
-	 // No memory management function required
-	 // So - need for destructor, copy constructor and assignment operator to be defined
+    
+    // No memory management function required
+    // So - need for destructor, copy constructor and assignment operator to be defined
+   
+    
 
-	 std::string grbFile() const                        { return m_grbFile; }
-	 std::string backgroundFile() const                 { return m_backgroundFile; }
-	 std::string mixedDir() const                       { return m_mixedDir; }
-	 std::string mixedFile() const                      { return m_mixedFile; }
-	 double grbOffsetTime() const                       { return m_grbOffsetTime; }
-	 const std::vector<PhotonInfo> &photonData() const  { return m_photonData; }
-	 long ngrb() const                                  { return m_nGrb; }
-	 long nbck() const                                  { return m_nBck; }
+    // Accessors
+    std::string grbFile() const                        { return m_grbFile; }
+    std::string backgroundFile() const                 { return m_backgroundFile; }
+    std::string mixedDir() const                       { return m_mixedDir; }
+    std::string mixedFile() const                      { return m_mixedFile; }
+    double grbOffsetTime() const                       { return m_grbOffsetTime; }
+    const std::vector<PhotonInfo> &photonData() const  { return m_photonData; }
+    long ngrb() const                                  { return m_nGrb; }
+    long nbck() const                                  { return m_nBck; }
+    
+    
+private:
+    /*!
+     * \brief Create background mixed events data.
+     */
+    void mix();
 
+    /*!
+     * \brief Read background data from specfied file.
+     */
+    void readBackground();
 
- private:
+    /*!
+     * \brief Read events data from specified file.
+     */
+    void readGRB();
 
-	 void mix();
-	 void readBackground();
-	 void readGRB();
-	 void readMixedData();
-	 void writeMixedData();
+    /*!
+     * \brief Read background mixed data.
+     */
+    void readMixedData();
 
+    /*!
+     * \brief Write background mixed data to a file for use by other programs */
+    void writeMixedData();
+    
 
-	 // data members
-	 std::string              m_grbFile;
-	 std::string              m_backgroundFile;
-	 std::string              m_mixedDir;
-	 std::string              m_mixedFile;
-	 double					  m_grbOffsetTime;
-	 std::vector<PhotonInfo>  m_photonData;
-	 long                     m_nGrb;
-	 long                     m_nBck;
+    
+    // data members
+    std::string              m_grbFile;              //! Name of file containing events data.
+    std::string              m_backgroundFile;       //! Name of file that lists background data.
+    std::string              m_mixedDir;             //! Directory where mixed file does or will reside.
+    std::string              m_mixedFile;            //! File that contains or will contain background mixed data.
+    double					 m_grbOffsetTime;        //! Time needed to offset events data times.
+    std::vector<PhotonInfo>  m_photonData;           //! Photon data list ((time,energy) pairs).
+    long                     m_nGrb;                 //! Number of photons in events data.
+    long                     m_nBck;                 //! Number of photons read from background data file.
 };
 
 
+
+/*!
+ * \brief This class implements method to sort data in time order.
+ */
 class timeCmp
 {
- public:
+public:
     bool operator()(PhotonInfo &data1, PhotonInfo &data2)
-	{
-	   return data1.time() < data2.time();    
-	}
+    {
+        return data1.time() < data2.time();    
+    }
 };
 
 
--- GRB/src/LatGRBAlert/LatGRBAlertConstants.h
+++ GRB/src/LatGRBAlert/LatGRBAlertConstants.h
@@ -1,5 +1,16 @@
-//
-// File: LatGRBAlertConstants.h
+/*! 
+ * \class LatGRBAlertConstants
+ * \brief Class instantiated to access general parameters and constants.
+ *  
+ * The namespace lattrigcst contains all the constant needed by the algorithm to determine
+ * trigger likelihoods.
+ *
+ * The namespace lattrigtypes contains typedef definitions used by the program.
+ *
+ * \author Jay Norris             jnorris@lheapop.gsfc.nasa.gov
+ * \author Sandhia Bansal         sandhiab@lheapop.gsfc.nasa.gov
+ */
+
 
 #ifndef LATGRBALERT_CONSTANTS_H
 #define LATGRBALERT_CONSTANTS_H 
@@ -10,29 +21,25 @@
 
 
 
-
-
-
-
 namespace lattrigcst
 {
-	const double  ratebck     = 4.07;
-	const double  degInPi     = 180.0 / M_PI;
-	const double  twosigdist  = 34./degInPi;
-
-//	const long    nrange      = 8L;
-	const long    nrange      = 80L;
-	const long    nmove       = nrange/4;
+    const double  ratebck     = 4.07;
+    const double  degInPi     = 180.0 / M_PI;
+    const double  twosigdist  = 34./degInPi;
+    
+    //	const long    nrange      = 8L;
+    const long    nrange      = 80L;
+    const long    nmove       = nrange/4;
 }
 
 
 
 namespace lattrigtypes
 {
-	typedef std::vector<std::vector<double> >::const_iterator DoubleVecConstIter;
-	typedef std::vector<double>::const_iterator DoubleConstIter;
-	typedef std::vector<double>::iterator DoubleIter;
-	typedef std::vector<double>::const_reverse_iterator DoubleConstRevIter;
+    typedef std::vector<std::vector<double> >::const_iterator DoubleVecConstIter;
+    typedef std::vector<double>::const_iterator DoubleConstIter;
+    typedef std::vector<double>::iterator DoubleIter;
+    typedef std::vector<double>::const_reverse_iterator DoubleConstRevIter;
 }
 
 #endif
