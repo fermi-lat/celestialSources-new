480d2877252f94ef458ed0aad26d7ae247a56ad1
jchiang <>
2004-06-29 16:38:16 +0000

    initial import of generic sources for use by flux, obsSim, Gleam, etc.

---
+++ genericSources/genericSources/mainpage.h
@@ -0,0 +1,20 @@
+// Mainpage for doxygen
+
+/** @mainpage package genericSources
+
+ @author James Chiang
+
+ @section intro Introduction
+
+ The sources in this package implement flux::ISpectrum sources to
+ provide photons from astrophysical sources for Gleam and
+ observationSim.
+
+ <hr>
+ @section notes Release Notes
+  release.notes
+
+ <hr>
+ @section requirements requirements
+ @verbinclude requirements
+*/
