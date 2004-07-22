df90847580482605cc4aa3c3fc9f3bd2178c6d1a
cvs2git <>
2004-07-22 16:32:18 +0000

    This commit was manufactured by cvs2svn to create branch 'GlastRelease- v4r7p3'.
    Sprout from GLAST 2004-07-22 16:32:17 UTC omodei 'Pulsar First import'
    Delete:
        .cvsignore
        GRB/.cvsignore
        GRB/cmt/NMake
        GRB/cmt/cleanup.bat
        GRB/cmt/requirements
        GRB/cmt/setup.bat
        GRB/src/GRB/GRBConstants.cxx
        GRB/src/GRB/GRBConstants.h
        GRB/src/GRB/GRBShell.cxx
        GRB/src/GRB/GRBShell.h
        GRB/src/GRB/GRBShock.cxx
        GRB/src/GRB/GRBShock.h
        GRB/src/GRB/GRBSim.cxx
        GRB/src/GRB/GRBSim.h
        GRB/src/GRB/GRBSpectrum.cxx
        GRB/src/GRB/GRBSpectrum.h
        GRB/src/test/GRBParam.txt
        SpectObj/SpectObj/SpectObj.h
        SpectObj/cmt/requirements
        SpectObj/src/SpectObj.cxx
        cmt/requirements
        doc/release.notes
        genericSources/.cvsignore
        genericSources/cmt/requirements
        genericSources/data/3c279Template.dat
        genericSources/data/CrabTemplate.dat
        genericSources/data/GemingaTemplate.dat
        genericSources/data/pks1622-297_Template.dat
        genericSources/doc/release.notes
        genericSources/genericSources/GaussianSource.h
        genericSources/genericSources/Isotropic.h
        genericSources/genericSources/MapSource.h
        genericSources/genericSources/PeriodicSource.h
        genericSources/genericSources/Pulsar.h
        genericSources/genericSources/SimpleTransient.h
        genericSources/genericSources/TransientTemplate.h
        genericSources/genericSources/mainpage.h
        genericSources/src/GaussianSource.cxx
        genericSources/src/Isotropic.cxx
        genericSources/src/MapSource.cxx
        genericSources/src/PeriodicSource.cxx
        genericSources/src/Pulsar.cxx
        genericSources/src/SimpleTransient.cxx
        genericSources/src/TransientTemplate.cxx
        genericSources/src/test/test.cxx
        genericSources/xml/source_library.xml
        src/mainpage.h

--- GRB/cmt/setup.bat
+++
@@ -1,9 +0,0 @@
-rem Setting GRB v0 in d:\users\srobinsn\code-current
-@echo off
-set CMTROOT=d:\ground\CMT\v1r10p20011126
-call %CMTROOT%\mgr\setup.bat
-
-set tempfile=%HOMEDRIVE%%HOMEPATH%tmpsetup.bat
-%CMTROOT%\%CMTBIN%\cmt.exe -quiet setup -bat -pack=GRB -version=v0 -path=d:\users\srobinsn\code-current %1 %2 %3 %4 %5 %6 %7 %8 %9 >%tempfile%
-if exist %tempfile% call %tempfile%
-if exist %tempfile% del %tempfile%
