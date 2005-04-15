ec7c197a8c26957d4fac81a281b2ebeb61a1cd8d
omodei <>
2005-04-15 17:44:31 +0000

    Update the documentation. Minor changes (remove cout and some commented lines)

--- GRB/src/GRB/GRBengine.h
+++ GRB/src/GRB/GRBengine.h
@@ -1,7 +1,9 @@
 /*!
  * \class GRBengine
  *
- * \brief This class permits to generate different kind of GRBs.
+ * \brief This class permits to generate several GRB.
+ *
+ * This class sets the parameters of the model, and create a vector of GRBShock.
  * 
  * \author Nicola Omodei       nicola.omodei@pi.infn.it 
  * \author Johann Cohen-Tanugi johann.cohen@pi.infn.it
@@ -18,11 +20,25 @@ class GRBengine
 {
   
  public:
+  /// Constructor. Get the galactic coordinates.
   GRBengine(Parameters *params);
+  ///  Default destructor
   ~GRBengine(){;}
+  /*!
+    Create a vector of GRBShock. The basic idea is:
+    - First it gets the parameters from the class Parameters.
+    - Start a loop checking on the duration of the burst
+    For a two given shells, one slower (front) and one faster (back), a GRBShok is created and a vector is filled.
     
+  */
   std::vector<GRBShock*> CreateShocksVector();
+  
+  /*
+    Returns the luminosity distance. The redshift is fixed to 1 since this method 
+    is used only for computing the Quantum Gravity effect.
+ */
   double GetDistance();
+ 
   inline std::pair<double,double> GetDirection(){return m_dir;}
  private:
   std::pair<double,double> m_dir;
--- GRB/src/mainpage.h
+++ GRB/src/mainpage.h
@@ -1,4 +1,4 @@
- /*! \mainpage GRB Package: simulating a transient source
+ /*! \mainpage The GRB Physical model: simulating a transient source
    
 The GRB package has been developed to provide the Glast simulator software 
 with a full fledged simulator of a physical transient source.
@@ -10,8 +10,8 @@ transient signals. It is interfaced to ROOT for possible visualization.
  
  This package contains four parts related to Gamma-Ray Bursts:
  -# The full physical simulator of GRB, based on the fireball model in 
-the internal shocks configuration.
- -# The phenomenological simulator of GRB signal. 
+ the internal shocks configuration.
+ -# The original phenomenological simulator of GRB signal. 
  -# The algorithm describing the LAT alert.
  
  \section physics Very short introduction to the physical model
@@ -21,14 +21,14 @@ Burst, because it is able to well reproduce the fast time variability
 observed in the GRB signal: a series of shells is injected in the circum 
 burst medium with different Lorentz factors. 
 When a faster shell reaches a slower one a shock occurs, and an accelerated 
-electron distribution is obtained due to the shock acceleration process. 
+electron distribution is obtained due to the shock acceleration process.
 Some of the energy dissipated during the shock is converted into a randomly 
-oriented magnetic field. The electrons can loose their energy via synchrotron
+oriented magnetic field. The electrons can lose their energy via synchrotron
  emission. The characteristic synchrotron spectrum is boosted (and beamed) 
-thanks to the Lorentz factor of the emitting material. 
-The higher energy part of a GRB spectrum can be obtained keeping into account 
-the possibility of Compton scattering of the synchrotron photons against the 
-electron accelerated by the shock (Inverse Compton Scattering).
+thanks to the Lorentz factor of the emitting material.
+The doppler beaming determines, in this model, the pulse shape.
+The higher energy part of a GRB spectrum is obtained by Inverse Compton scattering 
+the synchrotron photons against the electron accelerated by the shock (Self Synchrotron Compton).
 
 A short list of articles regarding the physics of GRB, 
 observations and theories can be found <a href="http://www.pi.infn.it/~omodei/biblio.html"> here</a>. 
@@ -43,17 +43,32 @@ It can be changed without recompiling the entire package.
 <br>
 It MUST be in /src/test directory of the GRB package. 
 <br>
-Depending on how GRBengine works, the sequence of shock that give up the GRB
-can be formed in different ways. 
-The File <a href="../../src/test/GRBParam.txt"> GRBParam.txt </a> contains information about:
-- The GRB number for initializing the Random number generator. Equal seeds are equal sequence of numbers
-- The galacic l and b coordinates. NOTE if l or b are < -180 than their value is extracted randomly.
-- The fluence in BATSE energy range (20 keV-1 MeV). NOTE if the value is zero than the fluence is set by Parameters::GetBATSEFluence()
-- The Number of shell
-- The variability of the central engine which determines the characteristic temporal scale.
-- The ratio between the maximum lorentz factor and the minimum lorentz factor of the shells
-- The energy at which \f$\nu f(\nu)\f$ peaks (approximately)
-- The ratio between the synchrotron and Inverse Compton energies (0-> Only synchrotron, 1 equal energy in synchrotron and in Ic);
+It contains the following observable quantities (typical values between parenthesis):
+- Fluence between 20 keV and 1 MeV, in erg/cm^2 (BATSE values)
+- Burst type: S=short bursts, L = long burst, R -> 25% S, 75% L
+- Cut-off energy \f$ E_{co} \f$, in GeV. (3, 10 GeV) 
+- Peak Energy \f$ E_{peak} \f$ of the synchrotron spectrum, in keV 
+(it is sampled from a log-normal distribution with mean 235 keV and sigma 1.75. Moreover, \f$ E_{peak,SHORT}=E_{peak,LONG}/2 \f$)
+- IC/Syn is the ratio between the Inverse Compton peak and the Synchrotron peak of the \f$ e^2 N(e)\f$ spectrum (0=pure synchrotron. < 10 typically)
+- GBM flag, if 1 the program generates a series of files to be used with the GBM software
+<br>
+Notice: if the value of some parameter is set to 0, the Parameters class extract randomly the values from the appropriate distributions.
+<br>
+The variability scale \f$ t_v \f$ is set by the observations, requiring that for short bursts it is approximately equal to the burst duration 
+(\f$ t_v\approx T_{90}\f$), whicle for long bursts it is sampled from the observed distribution of the FWHM: (\f$ t_v=10^{Gaus(0.0,0.5)}\f$).
+The variability time scale (which is directly related to the pulse width) is contsrained to a minimum: 
+The shortest variability time scale is a fraction (1/50) of the duration of the bursts: 
+long bursts will likely have long pulses instead of many short pulses.
+    
+    This quantities are converted into the parameters of the model by a series of relations:
+    
+    - \f$ \Gamma=40.5 * E_{co} \f$
+    - \f$ \frac{\Delta_0}{10^7}  =  13.6~(3~\alpha_B)(\frac{E_{tot}}{10^{52}})(\frac{3 \alpha_e \xi}{E_{co}})(\frac{E_{peak}}{100} t_v)^2 \f$
+    - \f$ r_0  =  2~c~t_v \f$
+    
+    The total energy is set (and fixed) to  \f$ 10^{52} \f$ erg, and \f$ \Gamma_M/\Gamma_m=2 \f$, so that: 
+    \f$ \Gamma_m = \frac{2~\Gamma}{\Gamma_M/\Gamma_m+1} \f$, and \f$ \Gamma_M= \frac{\Gamma_M}{\Gamma_m}\Gamma_m \f$ 
+    This is a model dependent way to correlate observables and model parameters.
 
 The parameters of the fireball are then computed in Parameters::ComputeParametersFromFile(std::string paramFile, int NGRB).
 
@@ -63,7 +78,6 @@ The parameters of the fireball are then computed in Parameters::ComputeParameter
 
 <br>
 \section test How-to use the test programs
-
 
 The GRB simulator can be use with several test program:
 
