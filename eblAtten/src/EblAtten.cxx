693627ae9495cb055375f675b42bb955a15cfd84
lreyes <>
2005-09-07 21:23:06 +0000

    New models: Primack05 and Kneiske_HighUV

--- eblAtten/src/EblAtten.cxx
+++ eblAtten/src/EblAtten.cxx
@@ -16,16 +16,18 @@ namespace IRB {
 
 std::map<EblModel, std::string> EblAtten::s_model_Ids;
 
-float calcSS(float energy, float redshift);
-float calcPB99(float energy, float redshift);
 float calcKneiske(float energy, float redshift);
+float calcPrimack05(float energy, float redshift);
+float calcKneiske_HighUV(float energy, float redshift);
+float calcSS(float energy, float redshift);
 
 EblAtten::EblAtten(EblModel model) : m_model(model) {
    if (s_model_Ids.size() == 0) {
+      s_model_Ids[Kneiske] = "Kneiske et al - Best Fit (2004)";
+      s_model_Ids[Primack05] = "Primack et al (2005)";
+      s_model_Ids[Kneiske_HighUV] = "Kneiske et al - High UV (2004)";
       s_model_Ids[Salamon_Stecker] = "Salamon & Stecker (1998)";
-      s_model_Ids[Primack_Bullock99] = "Primack & Bullock (1999)";
-      s_model_Ids[Kneiske] = "Kneiske et al. (2004)";
-   }
+      }
    if (s_model_Ids.find(model) == s_model_Ids.end()) {
       std::ostringstream message;
       message << "Invalid model ID: " << model << "\n"
@@ -39,15 +41,17 @@ EblAtten::EblAtten(EblModel model) : m_model(model) {
 }
 
 float EblAtten::operator()(float energy, float redshift) const {
-// Convert energy from MeV to TeV:
-   energy /= 1e6;
+// Convert energy from MeV to GeV:
+   energy /= 1e3;
    switch (m_model) {
-   case Salamon_Stecker:
-      return calcSS(energy, redshift);
-   case Primack_Bullock99:
-      return calcPB99(energy, redshift);
    case Kneiske:
       return calcKneiske(energy, redshift);
+   case Primack05:
+      return calcPrimack05(energy, redshift);
+   case Kneiske_HighUV:
+      return calcKneiske_HighUV(energy, redshift);
+   case Salamon_Stecker:
+      return calcSS(energy, redshift);
    }
    return 0;
 }
--- eblAtten/src/test/test.cxx
+++ eblAtten/src/test/test.cxx
@@ -15,20 +15,24 @@
 
 int main() {
 
-   IRB::EblAtten tau0(IRB::Kneiske), tau1(IRB::Salamon_Stecker),
-      tau2(IRB::Primack_Bullock99);
+   IRB::EblAtten tau0(IRB::Kneiske), tau1(IRB::Primack05),
+      tau2(IRB::Kneiske_HighUV), tau3(IRB::Salamon_Stecker);
 
    double emin(1e4), emax(5e5);
    int npts(50);
-   
-   double z(0.1);
+
+   double z [5] = {0., 0.5, 1., 2., 4.};
 
    double estep(std::log(emax/emin)/float(npts));
+
+  for (int j = 0; j < 5; j++)
    for (int i = 0; i < npts; i++) {
       double energy(emin*std::exp(i*estep));
-      std::cout << energy << "  " 
-                << tau0(energy, z) << "  "
-                << tau1(energy, z) << "  "
-                << tau2(energy, z) << std::endl;
+      std::cout << energy/1e3 << " GeV   "<< "  "
+                << "redshift = "<< z[j] << "   Tau = "
+                << tau0(energy, z[j]) << "  "
+                << tau1(energy, z[j]) << "  "
+                << tau2(energy, z[j]) << "  "
+		<< tau3(energy, z[j]) << std::endl;
    }
 }
