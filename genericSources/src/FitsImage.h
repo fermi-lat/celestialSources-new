ccb4f7d324440ef17dba907475212301d214416d
cvs2git <>
2005-10-31 21:03:06 +0000

    This commit was manufactured by cvs2svn to create tag 'v1r1'.
    Sprout from master 2002-07-30 12:39:34 UTC cohen 'redefining TESTJOBOPTIONS env. variable for GRB package'
    Cherrypick from master 2005-10-31 21:03:05 UTC jchiang 'AutoUpdate':
        ChangeLog
        Pulsar/Pulsar/PulsarConstants.h
        Pulsar/Pulsar/PulsarSim.h
        Pulsar/cmt/requirements
        Pulsar/data/PSRCRABTimeProfile.txt
        Pulsar/data/PSRVELATimeProfile.txt
        Pulsar/data/TestPulsarDataList.txt
        Pulsar/doc/DoxyFile
        Pulsar/doc/NJSnForm.jpg
        Pulsar/src/PulsarSim.cxx
        Pulsar/src/mainpage.h
        Pulsar/src/test/other/PulsarROOTtest.cxx
        Pulsar/xml/Pulsar_test_library.xml
        Pulsar/xml/Pulsar_user_library.xml
        SpectObj/SpectObj/SpectObj.h
        SpectObj/cmt/requirements
        SpectObj/doc/Doxyfile
        SpectObj/src/SpectObj.cxx
        celestialSources/SpectrumFactoryLoader.h
        cmt/requirements
        doc/release.notes
        genericSources/data/pks1622-297_Template.dat
        genericSources/data/testTemplate.dat
        genericSources/genericSources/TransientTemplate.h
        genericSources/src/PeriodicSource.cxx
        genericSources/src/Pulsar.cxx
        src/SpectrumFactoryLoader.cxx
        src/test/test.cxx
    Cherrypick from master 2005-02-08 00:26:44 UTC jchiang 'AutoUpdate':
        genericSources/ChangeLog
        genericSources/cmt/requirements
        genericSources/doc/release.notes
        genericSources/genericSources/MapCube.h
        genericSources/genericSources/MapSource.h
        genericSources/genericSources/SpectralTransient.h
        genericSources/src/FitsImage.cxx
        genericSources/src/FitsImage.h
        genericSources/src/MapCube.cxx
        genericSources/src/MapSource.cxx
        genericSources/src/SpectralTransient.cxx
        genericSources/src/TransientTemplate.cxx
        genericSources/src/Util.cxx
        genericSources/src/Util.h
        genericSources/src/mainpage.h
        genericSources/src/sources.in
        genericSources/src/test/test.cxx
        genericSources/xml/source_library.xml
    Cherrypick from master 2005-09-28 09:21:38 UTC razzano 'MJDREF fixed, PSRMURO Added':
        Pulsar/Pulsar/PulsarSpectrum.h
        Pulsar/data/PSRMUROTimeProfile.txt
        Pulsar/data/PulsarDataList.txt
        Pulsar/doc/release.notes
        Pulsar/src/PulsarSpectrum.cxx
    Cherrypick from master 2005-09-19 12:43:43 UTC razzano 'AutoUpdate':
        Pulsar/ChangeLog
    Cherrypick from master 2005-10-18 12:26:45 UTC omodei 'AutoUpdate':
        SpectObj/ChangeLog
    Cherrypick from GLAST 2004-06-29 16:38:16 UTC jchiang 'initial import of generic sources for use by flux, obsSim, Gleam, etc. ':
        .cvsignore
        GRB/.cvsignore
        GRB/cmt/NMake
        genericSources/.cvsignore
        genericSources/data/3c279Template.dat
        genericSources/data/CrabTemplate.dat
        genericSources/data/GemingaTemplate.dat
        genericSources/genericSources/GaussianSource.h
        genericSources/genericSources/Isotropic.h
        genericSources/genericSources/PeriodicSource.h
        genericSources/genericSources/Pulsar.h
        genericSources/genericSources/SimpleTransient.h
        genericSources/src/GaussianSource.cxx
        genericSources/src/Isotropic.cxx
        genericSources/src/SimpleTransient.cxx
        src/mainpage.h

---
+++ genericSources/src/FitsImage.h
@@ -0,0 +1,118 @@
+/** 
+ * @file FitsImage.h
+ * @brief Declaration of FitsImage class
+ * @authors J. Chiang
+ *
+ * $Header$
+ *
+ */
+
+#ifndef genericSources_FitsImage_h
+#define genericSources_FitsImage_h
+
+#include <sstream>
+#include <string>
+#include <vector>
+
+namespace genericSources {
+
+/** 
+ * @class FitsImage
+ *
+ * @brief A class for accessing FITS image data.
+ *
+ * @author J. Chiang
+ *    
+ * $Header$
+ *
+ */
+
+class FitsImage {
+    
+public:
+
+   FitsImage() {}
+
+   FitsImage(const std::string &fitsfile);
+
+   virtual ~FitsImage() {}
+
+   /// A vector of the image axes dimensions
+   virtual void getAxisDims(std::vector<int> &axisDims);
+
+   /// The names (CTYPEs) of the image axes
+   virtual void getAxisNames(std::vector<std::string> &axisNames);
+
+   /// Get a vector filled with axis abscissa points for the naxis-th
+   /// coordinate.
+   virtual void getAxisVector(unsigned int naxis, 
+                              std::vector<double> &axisVector);
+
+   /// This method computes arrays of longitude and latitude obtained
+   /// by traversing the image plane by column number then row.
+   virtual void getCelestialArrays(std::vector<double> &lonArray,
+                                   std::vector<double> &latArray);
+   
+   /// Get the pixel values.  They will be indexed by column, row,
+   /// then plane, i.e., indx = i + j*NAXIS1 + k*NAXIS1*NAXIS2.  Note
+   /// that each image plane is indexed starting at the lower left
+   /// (South-East) corner.
+   virtual void getImageData(std::vector<double> &imageData);
+
+   /// This returns the pixel solid angles.  Use of this method assumes
+   /// that m_axis[0] represents a longitudinal coordinate and that
+   /// m_axis[1] represents a latitudinal coordinate.  The pixel values
+   /// will be indexed by column then row, indx = i + j*NAXIS1.
+   virtual void getSolidAngles(std::vector<double> &solidAngles);
+
+   /// @return The HDU number of the specified extension.
+   static int findHdu(const std::string & fitsFile,
+                      const std::string & extension);
+
+   static void fitsReportError(int status, std::string routine="");
+
+protected:
+
+/** 
+ * @class AxisParams
+ * @brief Nested n-tuple class to represent FITS image axis information
+ */
+   class AxisParams {
+   public:
+      AxisParams() {}
+      ~AxisParams() {}
+      int size;
+      float refVal;
+      float step;
+      float refPixel;
+      std::string axisType;
+      std::string comment;
+      bool logScale;
+
+      /// Returns a vector of abscissa values based on the axis parameters.
+      void computeAxisVector(std::vector<double> &axisVector);
+   };
+
+   /// Interface to cfitsio routines.
+   void read_fits_image(std::string &filename, std::vector<AxisParams> &axes,
+                        std::vector<double> &image);
+
+   /// FITS file name.
+   std::string m_filename;
+
+   /// Descriptions for each image axis.
+   std::vector<AxisParams> m_axes;
+
+   /// Vectors of abscissa values for each axis.
+   std::vector< std::vector<double> > m_axisVectors;
+
+   /// The FITS image data
+   std::vector<double> m_image;
+
+   static std::string s_fitsRoutine;
+
+};
+
+} // namespace genericSources
+
+#endif // genericSources_FitsImage.h
