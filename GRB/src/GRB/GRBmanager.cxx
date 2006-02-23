295044bdd1b363f759d12f536d3b81f6ca4b80aa
omodei <>
2006-02-23 10:24:43 +0000

    Add data directory

--- GRB/src/GRB/GRBmanager.cxx
+++ GRB/src/GRB/GRBmanager.cxx
@@ -18,7 +18,7 @@ GRBmanager::GRBmanager(const std::string& params)
   : m_params(params)
 {
   m_Nbursts=0;
-  paramFile = "$(GRBROOT)/src/test/GRBParam.txt";
+  paramFile = "$(GRBROOT)/data/GRBParam.txt";
   facilities::Util::expandEnvVar(&paramFile);
   
   m_startTime       = parseParamList(params,0)+Spectrum::startTime();//m_startTime   = TMath::Max(0.,parseParamList(params,0));
