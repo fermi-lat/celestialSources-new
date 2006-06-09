a6cbf9b70a1ef447484061b73d526a4a05b2cf7f
jchiang <>
2006-06-09 05:32:20 +0000

    major refactoring of FileSpectrum internals

--- genericSources/genericSources/FileSpectrum.h
+++ genericSources/genericSources/FileSpectrum.h
@@ -1,66 +1,64 @@
 /** 
-* @file FileSpectrum.h
-* @brief FileSpectrum allows definition of a source based on a spectrum 
-* saved in a file. The file is currently assumed to have two columns: energy value
-* and flux (normalized or not). Flux value can be renormalized using the xml source 
-* description. Energy in the file can be in MeV or GeV, also to be decided in the xml
-* description.
-*
-*  $Header$
-*/
-#ifndef FileSpectrum_H
-#define FileSpectrum_H
-/** 
-* \class FileSpectrum
-*
-* \brief Spectrum that reads its differential spectrum from a table
-* \author Theodore Hierath
-* 
-* $Header $:
-*/
-//
-#include "flux/Spectrum.h"
-#include "facilities/Observer.h"
-#include <vector>
-#include <string>
+ * @file FileSpectrum.h
+
+ * @brief FileSpectrum allows definition of a source based on a
+ * spectrum saved in a file. The file is currently assumed to have two
+ * columns: energy value and flux (normalized or not). Flux value can
+ * be renormalized using the xml source description.  Energy in the
+ * file can only be in MeV consistent with GLAST/LAT standards.
+ *
+ *  $Header$
+ */
+
+#ifndef genericSources_FileSpectrum_H
+#define genericSources_FileSpectrum_H
+
 #include <map>
+#include <string>
+#include <vector>
 
+#include "flux/Spectrum.h"
 
-class FileSpectrum : public Spectrum
-{
+/** 
+ * \class FileSpectrum
+ *
+ * \brief Spectrum that reads its differential spectrum from a table
+ * \author Theodore Hierath
+ * 
+ * $Header$
+ */
+
+class FileSpectrum : public Spectrum {
+
 public:
-    /// params is the filename to read
-    FileSpectrum(const std::string& params);
-    
-    /// return total flux 
-    virtual double flux() const;
 
-    /// flux method to conform to FluxSvc spectrum standard
-    double flux (double time ) const;
-    
-    /// sample a single particle energy from the spectrum
-    virtual float operator() (float);
-    
-    virtual std::string title() const;
-    virtual const char * particleName() const;
-    inline  const char * nameOf() const {return "FileSpectrum";}
-    
-    std::vector<std::pair<double,double> > readFile(std::ifstream& input_file);
+   FileSpectrum(const std::string& params);
+   
+   /// @return total flux in #/m^2/s
+   virtual double flux() const;
 
+   /// @return total flux in #/m^2/s
+   virtual double flux (double time) const;
+    
+   /// sample a single particle energy from the spectrum
+   virtual float operator() (float);
+    
+   virtual std::string title() const;
+
+   virtual const char * particleName() const;
+
+   inline  const char * nameOf() const {return "FileSpectrum";}
+   
 private:
-    
-    
-    float m_fileflux;   // normalization from the file: can be overwritten in the xml
-    
-    bool m_inLog;
-    
-    typedef std::pair<double,double> efpair;
-    std::vector<efpair> integ_flux;
-    
-    
-    std::string initialization_document;
+
+// normalization from the file: can be overwritten in the xml
+   float m_fileflux;
+   
+   typedef std::pair<double, double> efpair_t;
+   std::vector<efpair_t> m_integ_flux;
+
+   std::vector<efpair_t> readFile(std::ifstream& input_file);
+
 };
 
-
-
-#endif // FileSpectrum_H
+#endif // genericeSources_FileSpectrum_H
