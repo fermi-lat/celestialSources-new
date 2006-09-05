1d0daac34b1ad7a8389f4191e18a52e66645dfe1
cvs2git <>
2006-09-05 20:08:32 +0000

    This commit was manufactured by cvs2svn to create branch 'GlastReleasev9r15'.
    Sprout from master 2006-09-05 20:08:30 UTC jchiang 'AutoUpdate'
    Cherrypick from master 2006-02-10 18:56:40 UTC razzano 'faint pulsar fix':
        Pulsar/ChangeLog
        Pulsar/Pulsar/PulsarSim.h
        Pulsar/Pulsar/PulsarSpectrum.h
        Pulsar/cmt/requirements
        Pulsar/doc/release.notes
        Pulsar/src/PulsarSim.cxx
        Pulsar/src/PulsarSpectrum.cxx
        Pulsar/src/test/other/PulsarROOTtest.cxx
        Pulsar/xml/Pulsar_user_library.xml
    Cherrypick from master 2006-02-13 00:53:15 UTC jchiang 'AutoUpdate':
        SpectObj/ChangeLog
        SpectObj/SpectObj/SpectObj.h
        SpectObj/cmt/requirements
        SpectObj/src/SpectObj.cxx
    Cherrypick from master 2005-11-09 00:32:59 UTC lreyes 'AutoUpdate':
        eblAtten/ChangeLog
    Cherrypick from master 2006-02-13 11:00:39 UTC omodei 'AutoUpdate':
        GRBobs/ChangeLog
    Cherrypick from master 2006-03-21 18:54:50 UTC usher 'AutoUpdate':
        GRB/ChangeLog
    Cherrypick from master 2006-05-17 18:54:27 UTC jchiang 'AutoUpdate':
        ChangeLog
    Cherrypick from GLAST 2005-08-29 21:23:09 UTC jchiang 'initial import':
        GRBobs/.cvsignore
        GRBobs/src/test/other/compile.C
        eblAtten/.cvsignore
        genericSources/data/3c279Template.dat
        genericSources/data/CrabTemplate.dat
        genericSources/data/GemingaTemplate.dat
        genericSources/genericSources/Isotropic.h
        genericSources/genericSources/PeriodicSource.h
        genericSources/genericSources/Pulsar.h
        genericSources/genericSources/SimpleTransient.h
        src/mainpage.h
    Delete:
        .cvsignore
        DarkMatter/.cvsignore
        DarkMatter/ChangeLog
        DarkMatter/DarkMatter/Profile.h
        DarkMatter/cmt/.cvsignore
        DarkMatter/cmt/requirements
        DarkMatter/release.notes
        DarkMatter/sources/DS_DC2/liste.dat
        DarkMatter/sources/DS_DC2/param.dat
        DarkMatter/sources/DS_DC2/res100MeVBisBis.txt
        DarkMatter/sources/DS_DC2/res10MeVBisBis.txt
        DarkMatter/sources/DS_DC2/rndm00000060.dat
        DarkMatter/sources/DS_DC2/rndm00000080.dat
        DarkMatter/sources/DS_DC2/rndm00000100.dat
        DarkMatter/sources/DS_DC2/rndm00000120.dat
        DarkMatter/sources/DS_DC2/rndm00000140.dat
        DarkMatter/sources/DS_DC2/rndm00000160.dat
        DarkMatter/sources/DS_DC2/rndm00000180.dat
        DarkMatter/sources/Profiles/1e10-85kpc_map.fits
        DarkMatter/src/LineOfSight.cxx
        DarkMatter/src/LineOfSight.h
        DarkMatter/src/Profile.cxx
        DarkMatter/src/globals.h
        DarkMatter/src/haloProfile.cxx
        DarkMatter/src/mainpage.h
        DarkMatter/src/makeMap.cxx
        DarkMatter/xml/darkmatter_library.xml
        GRBtemplate/.cvsignore
        GRBtemplate/ChangeLog
        GRBtemplate/GRBtemplate/GRBtemplateManager.h
        GRBtemplate/GRBtemplate/GRBtemplateSim.h
        GRBtemplate/cmt/Makefile
        GRBtemplate/cmt/cleanup.csh
        GRBtemplate/cmt/cleanup.sh
        GRBtemplate/cmt/requirements
        GRBtemplate/cmt/rh9_gcc32.make
        GRBtemplate/cmt/setup.csh
        GRBtemplate/cmt/setup.sh
        GRBtemplate/data/GRBTMP000000.dat
        GRBtemplate/data/GenerateFakeBurst.C
        GRBtemplate/data/pulse_002.dat
        GRBtemplate/data/test.dat
        GRBtemplate/src/GRBtemplateManager.cxx
        GRBtemplate/src/GRBtemplateSim.cxx
        GRBtemplate/src/test/other/GRBROOTtest.cxx
        GRBtemplate/xml/GRBtemplate_library.xml
        pySources/.cvsignore
        pySources/ChangeLog
        pySources/cmt/requirements
        pySources/doc/release.notes
        pySources/python/ebl_plot.py
        pySources/python/test_sources.py
        pySources/src/pySources.in

--- GRBtemplate/cmt/cleanup.sh
+++
@@ -1,9 +0,0 @@
-if test "${CMTROOT}" = ""; then
-  CMTROOT=/data0/glast/extlib/rh9_gcc32/CMT/v1r16p20040701; export CMTROOT
-fi
-. ${CMTROOT}/mgr/setup.sh
-tempfile=`${CMTROOT}/mgr/cmt build temporary_name -quiet`
-if test ! $? = 0 ; then tempfile=/tmp/cmt.$$; fi
-${CMTROOT}/mgr/cmt -quiet cleanup -sh -pack=GRBtemplate -version=v0r2 -path=/data0/glast/ScienceTools/celestialSources $* >${tempfile}; . ${tempfile}
-/bin/rm -f ${tempfile}
-
