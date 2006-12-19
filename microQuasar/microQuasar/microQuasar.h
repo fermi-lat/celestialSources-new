86e1bc90d6d0e06ef850267b21a01b1b9f4ca5da
cvs2git <>
2006-12-19 17:21:01 +0000

    This commit was manufactured by cvs2svn to create branch 'v0'.
    Cherrypick from GLAST 2005-08-29 21:23:09 UTC jchiang 'initial import':
        GRB/.cvsignore
        GRB/cmt/NMake
        GRB/cmt/cleanup.bat
        GRB/cmt/requirements
        GRB/cmt/setup.bat
        GRB/src/GRB/GRBConstants.cxx
        GRB/src/GRB/GRBConstants.h
        GRB/src/GRB/GRBShell.cxx
        GRB/src/GRB/GRBShell.h
        GRB/src/GRB/GRBShock.cxx
        GRB/src/GRB/GRBShock.h
        GRB/src/GRB/GRBSim.cxx
        GRB/src/GRB/GRBSim.h
        GRB/src/GRB/GRBSpectrum.cxx
        GRB/src/GRB/GRBSpectrum.h
        GRB/src/test/GRBParam.txt
        GRBobs/.cvsignore
        GRBobs/GRBobs/GRBobsConstants.h
        GRBobs/GRBobs/GRBobsPulse.h
        GRBobs/GRBobs/GRBobsSim.h
        GRBobs/GRBobs/GRBobsengine.h
        GRBobs/GRBobs/GRBobsmanager.h
        GRBobs/cmt/Makefile
        GRBobs/cmt/cleanup.csh
        GRBobs/cmt/cleanup.sh
        GRBobs/cmt/requirements
        GRBobs/cmt/rh9_gcc32.make
        GRBobs/cmt/setup.csh
        GRBobs/cmt/setup.sh
        GRBobs/src/GRBobsConstants.cxx
        GRBobs/src/GRBobsPulse.cxx
        GRBobs/src/GRBobsSim.cxx
        GRBobs/src/GRBobsengine.cxx
        GRBobs/src/GRBobsmanager.cxx
        GRBobs/src/test/GRBParam.txt
        GRBobs/src/test/other/GRBROOTtest.cxx
        GRBobs/src/test/other/compile.C
        GRBobs/xml/GRBobs_user_library.xml
        Pulsar/Pulsar/PulsarConstants.h
        Pulsar/Pulsar/PulsarSim.h
        Pulsar/Pulsar/PulsarSpectrum.h
        Pulsar/cmt/requirements
        Pulsar/src/PulsarSim.cxx
        Pulsar/src/PulsarSpectrum.cxx
        Pulsar/src/test/other/PulsarROOTtest.cxx
        SpectObj/SpectObj/SpectObj.h
        SpectObj/cmt/requirements
        SpectObj/src/SpectObj.cxx
        eblAtten/.cvsignore
        eblAtten/cmt/requirements
        eblAtten/doc/release.notes
        eblAtten/eblAtten/EblAtten.h
        eblAtten/src/EblAtten.cxx
        eblAtten/src/IRB_routines.cxx
        genericSources/.cvsignore
        genericSources/cmt/requirements
        genericSources/data/3c279Template.dat
        genericSources/data/CrabTemplate.dat
        genericSources/data/GemingaTemplate.dat
        genericSources/data/pks1622-297_Template.dat
        genericSources/doc/release.notes
        genericSources/genericSources/GaussianSource.h
        genericSources/genericSources/Isotropic.h
        genericSources/genericSources/MapSource.h
        genericSources/genericSources/PeriodicSource.h
        genericSources/genericSources/Pulsar.h
        genericSources/genericSources/SimpleTransient.h
        genericSources/genericSources/TransientTemplate.h
        genericSources/genericSources/mainpage.h
        genericSources/src/GaussianSource.cxx
        genericSources/src/Isotropic.cxx
        genericSources/src/MapSource.cxx
        genericSources/src/PeriodicSource.cxx
        genericSources/src/Pulsar.cxx
        genericSources/src/SimpleTransient.cxx
        genericSources/src/TransientTemplate.cxx
        genericSources/src/test/test.cxx
        genericSources/xml/source_library.xml
        pySources/.cvsignore
        pySources/cmt/requirements
        pySources/doc/release.notes
        pySources/python/test_sources.py
        pySources/src/pySources.in
    Cherrypick from master 2006-12-19 17:21:00 UTC burnett 'test package for Richard':
        microQuasar/.cvsignore
        microQuasar/cmt/.cvsignore
        microQuasar/cmt/Makefile
        microQuasar/cmt/NMake
        microQuasar/cmt/requirements
        microQuasar/microQuasar/microQuasar.h
        microQuasar/mq.txt
        microQuasar/src/microQuasar.cxx
        microQuasar/src/test/mq.txt
        microQuasar/src/test/test_main.cxx
        microQuasar/xml/mq.xml
    Cherrypick from master 2004-07-09 21:17:34 UTC jchiang 'use explicit version':
        cmt/requirements

---
+++ microQuasar/microQuasar/microQuasar.h
@@ -0,0 +1,176 @@
+/**
+* @file microQuasar.h
+* @brief A source class for the flux package that 
+* simulates the gamma emission of microQuasars
+* @author R. Dubois
+*
+* $Header$
+*/
+
+#ifndef microQuasar_h
+#define microQuasar_h
+
+#include <vector>
+
+#include "flux/Spectrum.h"
+
+/**
+* @class microQuasar
+*
+* @brief 
+* A source class for the flux package that
+* simulates the gamma emission of microQuasars
+* @author R. Dubois
+*
+*/
+
+class microQuasar : public Spectrum {
+
+public:
+
+	/// This constructor is required and used in FluxSource for
+	/// "SpectrumClass" sources.
+	microQuasar(const std::string &params);
+
+	virtual ~microQuasar(){}
+
+
+	/// @return Particle type, "gamma".
+	virtual const char * particleName() const {return "gamma";}
+
+	/// @return average differential flux integrated over energy (photons/m^2/sr).
+	/// @param time Simulation time in seconds.
+	virtual double flux(double) const {return 0;};
+
+	/// @return particle energy in MeV
+	/// @param xi uniform random deviate on the unir interval
+	virtual float operator()(float xi) const;
+
+	/// @return "Effective" solid angle (sr).
+	virtual double solidAngle() const {return 0;}
+
+	/// @return Title describing the spectrum.
+	virtual std::string title() const {return "microQuasar";}
+
+	/// @return Interval to the next event (seconds)
+	virtual double interval(double time);  
+
+	/// @return Photon energy (MeV).
+	virtual double energy(double time);
+
+	/// @return Photon direction in (zenith angle (degrees), azimuth (degrees,East=0,North=90deg)).
+	virtual std::pair<double, double> dir(double energy){
+		return std::make_pair(0,0);
+	}
+
+	/// @return energy and photon direction (zenith angle (degrees), azimuth (degrees,East=0,North=90deg)).
+	std::pair<double, std::pair<double, double> > photon();
+
+	/// function for generating the orbit modulation
+	void modulation(const double x, double& funcValue, double& derivValue);
+
+	/// fiddle of Numerical Recipe's rtsafe to avoid function passing
+	double rtsafe(const double x1, const double x2, const double xacc);
+
+	class OrbitalRegion {
+	public:
+		OrbitalRegion() {
+			m_spectralIndex[0] = 2.;
+			m_spectralIndex[1] = 1.7;
+			m_numOrbitalRegions = 2;
+			m_minOrbitalPhase = 0.25;
+			m_maxOrbitalPhase = 0.75;
+		};
+		~OrbitalRegion() {};
+		float getMinOrbitalPhase() {return m_minOrbitalPhase;}
+		float getMaxOrbitalPhase() {return m_maxOrbitalPhase;}
+		float getSpectralIndex(int region) {return m_spectralIndex[region];}
+		int findRegion(double time, float period);
+
+	private:
+		/// minimum orbital phase
+		float m_minOrbitalPhase;
+		/// max orbital phase
+		float m_maxOrbitalPhase;
+		/// number of orbital phases with different spectral indices
+		int m_numOrbitalRegions;
+		/// spectral index for this region
+		float m_spectralIndex[2];
+	};
+
+	class DiskCycleProperties {
+	public:
+		float getCycleDuration() {return m_cycleDuration;}
+		float getCycleDurationFluctuation() {return m_cycleDurationFluctuation;}
+
+	private:
+		/// disk cycle duration
+		float m_cycleDuration;
+		/// fractional fluctuation in cycle duration
+		float m_cycleDurationFluctuation;
+
+	};
+
+	class JetProperties {
+	public:
+		float getJetOnCycle() {return m_jetOnCycle;}
+		float getJetOnDuration() {return m_jetOnDuration;}
+
+	private:
+
+		/// onset of jet during cycle (in fraction of the duration)
+		float m_jetOnCycle;
+		/// fluctuation of jet on onset
+		float m_jetOnCycleFluctuation;
+		/// duration of jet
+		float m_jetOnDuration;
+		/// fluctuation of jet duration
+		float m_jetOnDurationFluctuation;
+		/// current spectral index
+
+	};
+protected:
+
+	void makeGrid(unsigned int n, double xmin, double xmax, 
+                 std::vector<double> &x, bool makeLog=false);
+   double interpolate(const std::vector<double> &x, 
+                      const std::vector<double> &y,
+                      double xx);
+
+
+private:
+	/// flux (ph s^-1 cm^-2)
+	float m_ftot;
+	float m_alt;
+	// minimum energy
+	double m_eMin;
+	/// max energy
+	double m_eMax;
+	/// period used 
+	float m_orbitalPeriod;
+	/// modulation over the period
+	float m_phi0;
+	float m_orbitalModulation;
+	float m_nTurns;
+	// current spectral index in use
+	float m_currentSpectralIndex;
+	/// randomly generated phase for generating interval
+	float m_randPhase;	
+	/// current time
+	double m_currentTime;
+	/// disk-cycle properties
+	DiskCycleProperties m_diskProperties;
+	/// jet timing properties
+	JetProperties m_jetProperties;
+	/// allow max two orbital regions for now
+	OrbitalRegion m_orbitalRegion;
+
+	std::vector<double> m_arrTimes;
+	std::vector<double> m_integralDist;
+	void computeIntegralDistribution();
+
+
+};
+
+#endif 
+
