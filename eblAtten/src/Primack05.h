c024fb7e015b91289916524230db29d96f92b827
jchiang <>
2008-01-08 19:39:55 +0000

    class to handle Primack05 model

---
+++ eblAtten/src/Primack05.h
@@ -0,0 +1,40 @@
+/**
+ * @file Primack05.h
+ * @brief Encapsulation of Primack, Bullock, Somerville (2005) astro-ph 0502177
+ * EBL model.
+ * 
+ * @author J. Chiang
+ *
+ * $Header$
+ */
+
+namespace IRB {
+
+/**
+ * @class Primack05
+ *
+ */
+
+class Primack05 {
+
+public:
+
+   static Primack05 & instance();
+   
+   float value(float energy, float redshift) const;
+
+protected:
+
+   static Primack05 * s_instance;
+
+   Primack05();
+
+private:
+
+   std::vector<float> m_zvalue;
+   std::vector<float> m_evalue;
+   std::vector< std::vector<float> > m_tauTables;
+
+};
+
+} // namespace IRB
