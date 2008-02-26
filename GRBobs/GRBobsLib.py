e4f541100bf89970cc1cf6b98da9a82a4ac141c2
glastrm <>
2008-02-26 16:43:31 +0000

    Add lib dependencies when creating a library

--- GRBobs/GRBobsLib.py
+++ GRBobs/GRBobsLib.py
@@ -1,5 +1,6 @@
 def generate(env, **kw):
-    env.Tool('addLibrary', library = ['GRBobs'], package = 'celestialSources/GRBobs')
+    if not kw.get('depsOnly',0):
+        env.Tool('addLibrary', library = ['GRBobs'], package = 'celestialSources/GRBobs')
     env.Tool('fluxLib')
     env.Tool('astroLib')
     env.Tool('SpectObjLib')
