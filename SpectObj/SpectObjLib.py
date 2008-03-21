e8002c46fc904b4c02e674a306d02dc179cacac7
cvs2git <>
2008-03-21 16:53:12 +0000

    This commit was manufactured by cvs2svn to create branch 'GlastReleasev13r9p23'.
    Sprout from master 2008-01-14 22:06:38 UTC burnett 'AutoUpdate'
    Cherrypick from master 2007-10-21 21:54:36 UTC jchiang 'AutoUpdate':
        genericSources/ChangeLog
        genericSources/cmt/requirements
        genericSources/doc/release.notes
        genericSources/genericSources/RadialSource.h
        genericSources/genericSources/SpectralTransient.h
        genericSources/src/RadialSource.cxx
        genericSources/src/SourcePopulation.cxx
        genericSources/src/SpectralTransient.cxx
        genericSources/src/mainpage.h
        genericSources/src/test/test.cxx
        genericSources/xml/source_library.xml
    Cherrypick from master 2008-02-26 16:43:31 UTC glastrm 'Add lib dependencies when creating a library':
        GRBtemplate/GRBtemplateLib.py
        Pulsar/PulsarLib.py
        Pulsar/doc/release.notes
        Pulsar/src/PulsarSpectrum.cxx
        SConscript
        celestialSourcesLib.py
        eblAtten/SConscript
        eblAtten/eblAttenLib.py
        microQuasar/SConscript
        microQuasar/cmt/requirements
        microQuasar/microQuasarLib.py
    Cherrypick from master 2007-09-27 17:23:59 UTC omodei 'AutoUpdate':
        SpectObj/ChangeLog
        SpectObj/cmt/requirements
        SpectObj/doc/release.notes
    Cherrypick from master 2008-03-21 16:52:34 UTC glastrm 'Register xml files with SCons':
        GRBtemplate/SConscript
        Pulsar/SConscript
    Cherrypick from master 2007-10-24 09:27:52 UTC omodei 'AutoUpdate':
        GRBobs/ChangeLog
    Cherrypick from master 2007-10-29 13:26:06 UTC cohen 'AutoUpdate':
        GRB/ChangeLog
    Cherrypick from master 2007-10-29 17:51:01 UTC jchiang 'AutoUpdate':
        ChangeLog
    Cherrypick from master 2008-01-08 20:11:59 UTC jchiang 'AutoUpdate':
        eblAtten/ChangeLog
    Cherrypick from GLAST 2005-08-29 21:23:09 UTC jchiang 'initial import':
        .cvsignore
        GRBobs/.cvsignore
        GRBobs/src/test/other/compile.C
        GRBtemplate/.cvsignore
        eblAtten/.cvsignore
        genericSources/data/3c279Template.dat
        genericSources/data/CrabTemplate.dat
        genericSources/data/GemingaTemplate.dat
        genericSources/genericSources/Isotropic.h
        genericSources/genericSources/PeriodicSource.h
        genericSources/genericSources/Pulsar.h
        genericSources/genericSources/SimpleTransient.h
        src/mainpage.h
    Delete:
        DarkMatter/.cvsignore
        DarkMatter/ChangeLog
        DarkMatter/DarkMatter/Profile.h
        DarkMatter/cmt/.cvsignore
        DarkMatter/cmt/requirements
        DarkMatter/release.notes
        DarkMatter/sources/DS_DC2/liste.dat
        DarkMatter/sources/DS_DC2/param.dat
        DarkMatter/sources/DS_DC2/res100MeVBisBis.txt
        DarkMatter/sources/DS_DC2/res10MeVBisBis.txt
        DarkMatter/sources/DS_DC2/rndm00000060.dat
        DarkMatter/sources/DS_DC2/rndm00000080.dat
        DarkMatter/sources/DS_DC2/rndm00000100.dat
        DarkMatter/sources/DS_DC2/rndm00000120.dat
        DarkMatter/sources/DS_DC2/rndm00000140.dat
        DarkMatter/sources/DS_DC2/rndm00000160.dat
        DarkMatter/sources/DS_DC2/rndm00000180.dat
        DarkMatter/sources/Profiles/1e10-85kpc_map.fits
        DarkMatter/src/LineOfSight.cxx
        DarkMatter/src/LineOfSight.h
        DarkMatter/src/Profile.cxx
        DarkMatter/src/globals.h
        DarkMatter/src/haloProfile.cxx
        DarkMatter/src/mainpage.h
        DarkMatter/src/makeMap.cxx
        DarkMatter/xml/darkmatter_library.xml
        GRB/GRBLib.py
        GRB/SConscript
        GRBobs/GRBobsLib.py
        GRBobs/SConscript
        SpectObj/SConscript
        SpectObj/SpectObjLib.py
        genericSources/SConscript
        genericSources/genericSourcesLib.py
        pySources/.cvsignore
        pySources/ChangeLog
        pySources/cmt/requirements
        pySources/doc/release.notes
        pySources/python/ebl_plot.py
        pySources/python/test_sources.py
        pySources/src/pySources.in

--- SpectObj/SpectObjLib.py
+++
@@ -1,9 +0,0 @@
-def generate(env, **kw):
-    env.Tool('addLibrary', library = ['SpectObj'], package = 'celestialSources/SpectObj')
-    env.Tool('eblAttenLib')
-    env.Tool('addLibrary', library = env['rootLibs'])
-    env.Tool('addLibrary', library = env['rootGuiLibs'])
-    env.Tool('addLibrary', library = env['clhepLibs'])
-
-def exists(env):
-    return 1
