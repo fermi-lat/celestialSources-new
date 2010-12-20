1d8119d36592a81e42cce639d750dcc3cf1489f8
cvs2git <>
2010-12-20 19:33:36 +0000

    This commit was manufactured by cvs2svn to create branch 'GlastReleasev18r8p8'.
    Sprout from master 2010-12-20 19:32:04 UTC jrb 'AutoUpdate'
    Cherrypick from master 2008-06-19 22:20:51 UTC glastrm 'Added Author and Version fields':
        SpectObj/SConscript
        SpectObj/SpectObj/SpectObj.h
        SpectObj/cmt/requirements
        SpectObj/doc/release.notes
        SpectObj/src/SpectObj.cxx
    Cherrypick from master 2008-03-19 21:28:01 UTC glast 'AutoUpdate':
        SpectObj/ChangeLog
    Cherrypick from master 2010-02-22 20:39:58 UTC jrb 'AutoUpdate':
        GRBobs/ChangeLog
    Cherrypick from master 2010-02-22 20:40:53 UTC jrb 'AutoUpdate':
        Pulsar/ChangeLog
    Cherrypick from master 2010-02-22 20:41:41 UTC jrb 'AutoUpdate':
        genericSources/ChangeLog
    Cherrypick from master 2010-02-22 20:42:08 UTC jrb 'AutoUpdate':
        eblAtten/ChangeLog
    Cherrypick from master 2010-02-22 20:42:55 UTC jrb 'AutoUpdate':
        microQuasar/ChangeLog
    Cherrypick from master 2010-06-03 14:45:21 UTC jchiang 'AutoUpdate':
        ChangeLog
    Cherrypick from master 2010-12-20 05:55:45 UTC cohen 'AutoUpdate':
        GRB/ChangeLog
    Cherrypick from GLAST 2004-12-06 10:26:02 UTC omodei 'First import of new GRB obs sim':
        GRBobs/src/test/other/compile.C
        genericSources/data/3c279Template.dat
        genericSources/data/CrabTemplate.dat
        genericSources/data/GemingaTemplate.dat
        genericSources/genericSources/PeriodicSource.h
        genericSources/genericSources/Pulsar.h
        genericSources/genericSources/SimpleTransient.h
        src/mainpage.h
    Delete:
        DarkMatter/.cvsignore
        DarkMatter/ChangeLog
        DarkMatter/DarkMatter/Profile.h
        DarkMatter/cmt/.cvsignore
        DarkMatter/cmt/requirements
        DarkMatter/release.notes
        DarkMatter/sources/DS_DC2/liste.dat
        DarkMatter/sources/DS_DC2/param.dat
        DarkMatter/sources/DS_DC2/res100MeVBisBis.txt
        DarkMatter/sources/DS_DC2/res10MeVBisBis.txt
        DarkMatter/sources/DS_DC2/rndm00000060.dat
        DarkMatter/sources/DS_DC2/rndm00000080.dat
        DarkMatter/sources/DS_DC2/rndm00000100.dat
        DarkMatter/sources/DS_DC2/rndm00000120.dat
        DarkMatter/sources/DS_DC2/rndm00000140.dat
        DarkMatter/sources/DS_DC2/rndm00000160.dat
        DarkMatter/sources/DS_DC2/rndm00000180.dat
        DarkMatter/sources/Profiles/1e10-85kpc_map.fits
        DarkMatter/src/LineOfSight.cxx
        DarkMatter/src/LineOfSight.h
        DarkMatter/src/Profile.cxx
        DarkMatter/src/globals.h
        DarkMatter/src/haloProfile.cxx
        DarkMatter/src/mainpage.h
        DarkMatter/src/makeMap.cxx
        DarkMatter/xml/darkmatter_library.xml
        pySources/.cvsignore
        pySources/ChangeLog
        pySources/cmt/requirements
        pySources/doc/release.notes
        pySources/python/ebl_plot.py
        pySources/python/test_sources.py
        pySources/src/pySources.in

--- SpectObj/SpectObj/SpectObj.h
+++ SpectObj/SpectObj/SpectObj.h
@@ -82,7 +82,6 @@ class SpectObj
   photon GetPhoton(double t0, double enph);
   
   void SetAreaDetector(double AreaDetector=6.0);
-  inline void SetFluxFactor(double FluxFactor=1.0) {m_FluxFactor = FluxFactor;};
     
   inline double GetAreaDetector() { return m_AreaDetector; }
   double flux(double time, double enph);
@@ -104,8 +103,6 @@ class SpectObj
  private:
   int counts;
   double  m_AreaDetector;
-  double  m_FluxFactor;
-
 #if 0 //THB
   TRandom *m_SpRandGen;
 #else
