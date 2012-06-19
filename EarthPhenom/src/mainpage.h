320db1bf47be082a62dd2b17749875808807e230
bechtol <>
2012-06-19 21:59:11 +0000

    Initial import

---
+++ EarthPhenom/src/mainpage.h
@@ -0,0 +1,19 @@
+/*! \mainpage EarthPhenom Package: simulating gamma-ray emission from the Earth source for LAT
+* 
+*
+* \author Keith Bechtol  bechtol@stanford.edu 
+*
+* @section introduction Introduction
+*
+* EarthPhenom is a package that simulates gamma-ray emission from the Earth.
+* The model contain two components:
+* - Earth limb region (nadir angles 50-70 deg)
+* - Inner Earth region (nadir angles < 50 deg)
+*
+* There are 3 parameters for each model, supplied in the source xml file:
+*
+* Scale - Scale factor for overall normalization ('1' corresponds to LAT-observed normalization)
+* EMin - Minimum simulated energy (Mev)
+* EMax - Maximum simulated energy (MeV)
+*/
+
