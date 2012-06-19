7e32ce6183662744ba0d29a0b2d6d46f23f42e41
cvs2git <>
2012-06-19 21:59:12 +0000

    This commit was manufactured by cvs2svn to create tag 'KCB'.
    Sprout from v0 2012-06-19 21:59:11 UTC bechtol 'Initial import'
    Delete:
        DarkMatter/.cvsignore
        DarkMatter/cmt/Makefile
        DarkMatter/cmt/cleanup.csh
        DarkMatter/cmt/cleanup.sh
        DarkMatter/cmt/requirements
        DarkMatter/cmt/rh9_gcc32.make
        DarkMatter/cmt/setup.csh
        DarkMatter/cmt/setup.sh
        DarkMatter/release.notes
        DarkMatter/sources/DM_10GeV.txt
        DarkMatter/sources/DM_20GeV.txt
        DarkMatter/sources/DM_30GeV.txt
        DarkMatter/sources/darksusy_example.txt
        DarkMatter/src/mainpage.h
        DarkMatter/xml/dark_matter_library.xml
        GRB/.cvsignore
        GRB/cmt/NMake
        GRB/cmt/cleanup.bat
        GRB/cmt/requirements
        GRB/cmt/setup.bat
        GRB/src/GRB/GRBConstants.cxx
        GRB/src/GRB/GRBConstants.h
        GRB/src/GRB/GRBShell.cxx
        GRB/src/GRB/GRBShell.h
        GRB/src/GRB/GRBShock.cxx
        GRB/src/GRB/GRBShock.h
        GRB/src/GRB/GRBSim.cxx
        GRB/src/GRB/GRBSim.h
        GRB/src/GRB/GRBSpectrum.cxx
        GRB/src/GRB/GRBSpectrum.h
        GRB/src/test/GRBParam.txt
        GRBobs/.cvsignore
        GRBobs/GRBobs/GRBobsConstants.h
        GRBobs/GRBobs/GRBobsPulse.h
        GRBobs/GRBobs/GRBobsSim.h
        GRBobs/GRBobs/GRBobsengine.h
        GRBobs/GRBobs/GRBobsmanager.h
        GRBobs/cmt/Makefile
        GRBobs/cmt/cleanup.csh
        GRBobs/cmt/cleanup.sh
        GRBobs/cmt/requirements
        GRBobs/cmt/rh9_gcc32.make
        GRBobs/cmt/setup.csh
        GRBobs/cmt/setup.sh
        GRBobs/src/GRBobsConstants.cxx
        GRBobs/src/GRBobsPulse.cxx
        GRBobs/src/GRBobsSim.cxx
        GRBobs/src/GRBobsengine.cxx
        GRBobs/src/GRBobsmanager.cxx
        GRBobs/src/test/GRBParam.txt
        GRBobs/src/test/other/GRBROOTtest.cxx
        GRBobs/src/test/other/compile.C
        GRBobs/xml/GRBobs_user_library.xml
        Pulsar/Pulsar/PulsarConstants.h
        Pulsar/Pulsar/PulsarSim.h
        Pulsar/Pulsar/PulsarSpectrum.h
        Pulsar/cmt/requirements
        Pulsar/src/PulsarSim.cxx
        Pulsar/src/PulsarSpectrum.cxx
        Pulsar/src/test/other/PulsarROOTtest.cxx
        SpectObj/SpectObj/SpectObj.h
        SpectObj/cmt/requirements
        SpectObj/src/SpectObj.cxx
        cmt/requirements
        eblAtten/.cvsignore
        eblAtten/cmt/requirements
        eblAtten/doc/release.notes
        eblAtten/eblAtten/EblAtten.h
        eblAtten/src/EblAtten.cxx
        eblAtten/src/IRB_routines.cxx
        genericSources/.cvsignore
        genericSources/cmt/requirements
        genericSources/data/3c279Template.dat
        genericSources/data/CrabTemplate.dat
        genericSources/data/GemingaTemplate.dat
        genericSources/data/pks1622-297_Template.dat
        genericSources/doc/release.notes
        genericSources/genericSources/GaussianSource.h
        genericSources/genericSources/Isotropic.h
        genericSources/genericSources/MapSource.h
        genericSources/genericSources/PeriodicSource.h
        genericSources/genericSources/Pulsar.h
        genericSources/genericSources/SimpleTransient.h
        genericSources/genericSources/TransientTemplate.h
        genericSources/genericSources/mainpage.h
        genericSources/src/GaussianSource.cxx
        genericSources/src/Isotropic.cxx
        genericSources/src/MapSource.cxx
        genericSources/src/PeriodicSource.cxx
        genericSources/src/Pulsar.cxx
        genericSources/src/SimpleTransient.cxx
        genericSources/src/TransientTemplate.cxx
        genericSources/src/test/test.cxx
        genericSources/xml/source_library.xml
        microQuasar/.cvsignore
        microQuasar/cmt/.cvsignore
        microQuasar/cmt/Makefile
        microQuasar/cmt/NMake
        microQuasar/cmt/requirements
        microQuasar/microQuasar/microQuasar.h
        microQuasar/mq.txt
        microQuasar/src/microQuasar.cxx
        microQuasar/src/test/mq.txt
        microQuasar/src/test/test_main.cxx
        microQuasar/xml/mq.xml
        pySources/.cvsignore
        pySources/cmt/requirements
        pySources/doc/release.notes
        pySources/python/test_sources.py
        pySources/src/pySources.in

--- GRBobs/cmt/cleanup.sh
+++
@@ -1,9 +0,0 @@
-if test "${CMTROOT}" = ""; then
-  CMTROOT=/data0/glast/extlib/CMT/v1r14p20031120; export CMTROOT
-fi
-. ${CMTROOT}/mgr/setup.sh
-tempfile=`${CMTROOT}/mgr/cmt build temporary_name -quiet`
-if test ! $? = 0 ; then tempfile=/tmp/cmt.$$; fi
-${CMTROOT}/mgr/cmt -quiet cleanup -sh -pack=GRBobs -version=v0 -path=/data0/glast/GRBSimul/celestialSources $* >${tempfile}; . ${tempfile}
-/bin/rm -f ${tempfile}
-
