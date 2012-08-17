5d724a2e395d48bbedb7c3539a7c405e34a854a2
jrb <>
2012-08-17 19:50:31 +0000

    patches for Windows no-install-headers

--- GRB/GRBLib.py
+++ GRB/GRBLib.py
@@ -2,11 +2,23 @@
 def generate(env, **kw):
     if not kw.get('depsOnly',0):
         env.Tool('addLibrary', library = ['GRB'], package = 'celestialSources/GRB')
+        # No includes so don't need to add ourselves to include path
+        if env['PLATFORM'] == "win32" and env.get('CONTAINERNAME','') == 'GlastRelease':
+            env.Tool('findPkgPath', package = 'flux') 
+            env.Tool('findPkgPath', package = 'astro') 
+            env.Tool('findPkgPath', package = 'facilities') 
+
     env.Tool('fluxLib')
     env.Tool('astroLib')
     env.Tool('SpectObjLib')
     env.Tool('addLibrary', library = env['rootLibs'])
     env.Tool('addLibrary', library = env['rootGuiLibs'])
+    if kw.get('incsOnly', 0) == 1:
+        env.Tool('findPkgPath', package = 'flux') 
+        env.Tool('findPkgPath', package = 'astro') 
+        env.Tool('findPkgPath', package = 'facilities') 
+        env.Tool('findPkgPath', package = 'SpectObj') 
+
 
 def exists(env):
     return 1
--- GRBobs/GRBobsLib.py
+++ GRBobs/GRBobsLib.py
@@ -1,11 +1,23 @@
 def generate(env, **kw):
     if not kw.get('depsOnly',0):
         env.Tool('addLibrary', library = ['GRBobs'], package = 'celestialSources/GRBobs')
+        if env['PLATFORM'] == "win32" and env.get('CONTAINERNAME','') == 'GlastRelease':
+            env.Tool('findPkgPath', package = 'flux') 
+            env.Tool('findPkgPath', package = 'astro') 
+            env.Tool('findPkgPath', package = 'facilities') 
+            env.Tool('findPkgPath', package = 'GRBobs') 
+            env.Tool('findPkgPath', package = 'SpectObj') 
+
     env.Tool('fluxLib')
     env.Tool('astroLib')
     env.Tool('SpectObjLib')
     env.Tool('addLibrary', library = env['rootLibs'])
     env.Tool('addLibrary', library = env['rootGuiLibs'])
+    if kw.get('incsOnly', 0) == 1: 
+        env.Tool('findPkgPath', package = 'facilities') 
+        env.Tool('findPkgPath', package = 'astro')
+        env.Tool('findPkgPath', package = 'flux')
+        env.Tool('findPkgPath', package = 'SpectObj')
 
 def exists(env):
     return 1
