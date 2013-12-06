7be17457c960c02a74b3e6f40e9686d7cfa2a7d1
jchiang <>
2013-12-06 15:20:45 +0000

    add AsciiTableModel and Gilmore et al 2012 models

--- eblAtten/eblAttenLib.py
+++ eblAtten/eblAttenLib.py
@@ -1,8 +1,16 @@
 def generate(env, **kw):
     if not kw.get('depsOnly',0):
         env.Tool('addLibrary', library = ['eblAtten'])
-	if env['PLATFORM'] == 'win32' and env.get('CONTAINERNAME','') == 'GlastRelease':
+        if (env['PLATFORM'] == 'win32' and
+            env.get('CONTAINERNAME','') == 'GlastRelease'):
 	    env.Tool('findPkgPath', package = 'eblAtten') 
-    #  No need for incsOnly section since eblAtten references no other pkgs
+            env.Tool('findPkgPath', package = 'facilities') 
+            env.Tool('findPkgPath', package = 'st_facilities')
+    env.Tool('facilitiesLib')
+    env.Tool('st_facilitiesLib')
+    if kw.get('incsOnly', 0) == 1: 
+        env.Tool('findPkgPath', package = 'facilities') 
+        env.Tool('findPkgPath', package = 'st_facilities') 
+
 def exists(env):
     return 1
